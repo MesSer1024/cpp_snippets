d77d56f90fdd3992afd47afe34d21f3a414a5bc1
Daniel Dahlkvist <messer_@hotmail.com>
2018-09-14 14:56:07 +0200

    rename and restructure

--- source/Source.cpp
+++ source/Source.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include "StructWithBitfields.h"
 
-void runStructWithBitfields();
+void testStructWithBitfields()
+{
+	u32 size = sizeof(StructWithBitfields);
+	u32 alignment = alignof(StructWithBitfields);
+
+	u32 smartSize = sizeof(SmartStructWithBitfields);
+	u32 smartAlignment = alignof(SmartStructWithBitfields);
+
+	printf("sz | align  name_of_struct\n");
+	printf("--------------------------\n");
+	printf("%u | %u -- StructWithBitfields\n", size, alignment);
+	printf("%u | %u -- SmartStructWithBitfields\n", smartSize, smartAlignment);
+}
 
 int main()
 {
 	using namespace std;
 
-	runStructWithBitfields();
+	testStructWithBitfields();
 	cin.get();
 	return 0;
 }
